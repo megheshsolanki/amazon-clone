42f37ec5c69cc87f2500385a2ed44b45a4a7edf4
megheshsolanki <megheshsolanki@gmail.com>
2023-05-21 02:09:58 +0530

    day 1 - Home & Header Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  const Router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <Header />
+          <Home />
+        </>
+      ),
+    },
+    {
+      path: "/checkout",
+      element: (
+        <>
+          <Header />
+          <h1>Checkout, to be continued</h1>
+        </>
+      ),
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="app">
+        <RouterProvider router={Router}/>
+      </div>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCfNDEugg_mANew7PNaEroWC-ZpWGDxnlA",
+    authDomain: "clone-462a7.firebaseapp.com",
+    projectId: "clone-462a7",
+    storageBucket: "clone-462a7.appspot.com",
+    messagingSenderId: "606060598835",
+    appId: "1:606060598835:web:a1d949e4c4c983716b3546",
+    measurementId: "G-GFF8Z9N8G6"
+  };
